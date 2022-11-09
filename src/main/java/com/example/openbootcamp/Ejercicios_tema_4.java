2b01e30af1bb169b13d6ae1b889b8f809998b946
federico González <fedeg130@gmail.com>
2022-11-09 15:37:59 -0300

    ejercicio tema 4

---
+++ src/main/java/com/example/openbootcamp/Ejercicios_tema_4.java
@@ -0,0 +1,82 @@
+package com.example.openbootcamp;
+
+public class Ejercicios_tema_4 {
+    //If
+
+   /* public static void main(String[] args) {
+        var numeroIf = 0;
+
+        if (numeroIf > 0) {
+            System.out.println("Es positivo");
+        } else if (numeroIf < 0) {
+            System.out.println("Es negativo");
+        } else {
+            System.out.println("Es 0");
+        }
+    }*/
+
+
+    //While
+
+    /*public static void main(String[] args){
+        int numeroWhile = 0;
+
+        while(numeroWhile < 3){
+            System.out.println(numeroWhile);
+            numeroWhile = numeroWhile + 1;
+        }
+
+
+    }*/
+
+    //Do While
+
+    /*public static void main(String[] args){
+        int numeroWhile = 0;
+
+        do {
+            System.out.println(numeroWhile);
+            numeroWhile = numeroWhile + 1;
+            break;
+        } while(numeroWhile < 3);
+
+    }*/
+
+    //For
+
+    /*public static void main(String[] args){
+        for (int numeroFor = 0; numeroFor <= 3; numeroFor = numeroFor + 1) {
+            System.out.println(numeroFor);
+        }
+
+    }*/
+
+    //Swith
+
+    /*public static void main(String[] args){
+        var estacion = "verano";
+
+        switch(estacion){
+            case "invierno":
+                System.out.println("es invierno");
+                break;
+            case "otoño":
+                System.out.println("es otoño");
+                break;
+            case "primavera":
+                System.out.println("es primavera");
+                break;
+            case "verano":
+                System.out.println("es verano");
+        }
+
+
+
+    }*/
+
+
+
+
+
+
+}
